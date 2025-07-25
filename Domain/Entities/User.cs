0ad265ac229a547c7c7fef8f0f0a5119d5595dba
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-07-25 07:31:01 +0100

    refactor

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -4,8 +4,6 @@ namespace Moneybox.Domain.Entities
 {
     public class User(Guid id, Name fullName, Email email) : Aggregate(id)
     {
-        private User() : this(Guid.Empty, new Name(), new Email()) { }
-    
         public Name FullName { get; private set; } = fullName;
         public Email Email { get; private set; } = email;
     }
